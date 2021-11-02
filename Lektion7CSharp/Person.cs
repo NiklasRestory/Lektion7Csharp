81c014786b68102d65523591d4759629e3eb56df
NiklasRestory <niklas.cullberg@restory.ai>
2021-11-02 10:44:26 +0100

    Did something with Person.

--- Lektion7CSharp/Person.cs
+++ Lektion7CSharp/Person.cs
@@ -7,6 +7,7 @@ namespace Lektion7CSharp
     class Person
     {
         public string Name { get; set; }
+        public int Age { get; set; }
         public string Email { get; set; }
 
         public Person(string name)
