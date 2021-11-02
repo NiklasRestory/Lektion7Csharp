73ed51aef8d98e038e054df90def4230b3932952
NiklasRestory <niklas.cullberg@restory.ai>
2021-11-02 09:41:49 +0100

    Created a C# project.
    Created console application, made an int with value of 5, printed the number.

---
+++ Lektion7CSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lektion7CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int five = 5;
+            Console.WriteLine("Printing number " + five);
+        }
+    }
+}
